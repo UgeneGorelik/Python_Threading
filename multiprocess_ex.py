a2680908db5a8e45823eb719900ad86449d036b4
Ivgeny Gorelik <34338008+UgeneGorelik@users.noreply.github.com>
2017-12-25 13:17:56 +0200

    Added Correct Comments

--- multiprocess_ex.py
+++ multiprocess_ex.py
@@ -1,5 +1,14 @@
-import  multiprocessing
-import time
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# This example will show how to use multiprocessing
+# it will run 2 functions in parallel as a process
+#-----------------------------------------------------------------------------
+
+
+import multiprocessing
+
+
+#Function to calculate Square
 
 def calc_sq(numbers):
     print("square")
@@ -7,11 +16,26 @@ def calc_sq(numbers):
 
         print("sq " +str(x*x))
 
+#Function to calculate Cube
+
 def calc_cu(numbers):
     print("cube")
     for x in numbers:
 
         print("cube"+ str(x*x*x))
 
-arr=[1,2,3]
-p1 = multiprocessing.Process(target=calc_sq,args=(arr,))
+
+if __name__ == '__main__':
+
+    # initialising an array
+    arr=[1,2,3]
+
+    #defining a process to run as multiprocess
+
+    p1 = multiprocessing.Process(target=calc_sq,args=(arr,))
+    p2 = multiprocessing.Process(target=calc_cu, args=(arr,))
+
+    #Starting the process
+
+    p1.start()
+    p2.start()
